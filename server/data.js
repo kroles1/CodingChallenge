7961cd48e7a2a4bbe354f96679cb3278ffe29d99
kroles1 <korneliaoles@gmail.com>
2022-09-28 12:04:46 +0200

    dog.js, data.js, app.get

---
+++ server/data.js
@@ -0,0 +1,10 @@
+const dogs = [
+    {id: 1, breed: 'Australian Shepherd', snippet: 'The Australian Shepherd is good-natured with an even disposition. It may be somewhat reserved when meeting people for the first time, but there should never be any display of shyness or aggression.', link: 'https://www.pedigree.com.ph/dog-breeds/australian-shepherd'},
+    {id: 2, breed: 'Collie', snippet: 'Collies are extremely intelligent animals that genuinely enjoy human company. They are relatively easy to train and are blessed with an energetic, bouncy spirit.', link: 'https://www.pedigree.com.ph/dog-breeds/collie'},
+    {id: 3, breed: 'Golden Retriever', snippet: 'Golden Retrievers are one of the most fun-loving dogs and love showering affection on their owners. A Golden Retriever\'s temperament is extremely friendly, and they behave well around children.', link: 'https://www.pedigree.com.ph/dog-breeds/golden-retriever'},
+    {id: 4, breed: 'Pug', snippet: 'A pug\'s temperament is always friendly and cheerful. They make great companion dogs and love being by their owner\'s side all the time.', link: 'https://www.pedigree.com.ph/dog-breeds/pug'},
+    {id: 5, breed: 'Yorkshire Terrier', snippet: 'Most Yorkshire Terriers are alert, active and inquisitive. They can be determined, but are faithful and loyal to their human family. They thrive on human companionship and will happily be included in family activities.', link: 'https://www.pedigree.com.ph/dog-breeds/yorkshire-terrier'},
+]
+    
+
+module.exports = dogs;
